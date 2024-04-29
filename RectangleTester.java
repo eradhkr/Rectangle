6c026f7246fcedf5628dd3ebd1d6aa880138676a
eradhkr <166973248+eradhkr@users.noreply.github.com>
2024-04-28 22:53:15 -0700

    initial commit

---
+++ RectangleTester.java
@@ -0,0 +1,11 @@
+public class RectangleTester
+{
+    public static void main(String[] args)
+    {
+        
+         Rectangle r1 = new Rectangle(10, 2);
+         System.out.println(r1);
+         Rectangle r2 = new Rectangle(5, 15);
+         System.out.println(r2);
+    }
+}
